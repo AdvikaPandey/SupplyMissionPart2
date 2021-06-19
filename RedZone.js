09d8317d4cfc41166bc8df9b45d17887e436f871
AdvikaPandey <69717028+AdvikaPandey@users.noreply.github.com>
2021-06-19 19:36:22 +0530

    Update RedZone.js

--- RedZone.js
+++ RedZone.js
@@ -1,9 +1,7 @@
 class RedZone{
     constructor(x, y, width, height) {
         var options = {
-            'restitution':0.8,
-            'friction':1.5,
-            'density':1.2,
+            isStatic: true
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
